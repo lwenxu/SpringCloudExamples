40592c627ab7a17ef645caaa060ec23b6fe51cf0
lwen <1405766743@qq.com>
2018-05-28 22:48:32 +0800

    主配置

---
+++ dept-config-3344/src/main/java/lwen/DeptConfigApp3344.java
@@ -0,0 +1,11 @@
+package lwen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeptConfigApp3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(DeptConfigApp3344.class, args);
+    }
+}
